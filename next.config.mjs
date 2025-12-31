b3917f6479d05579f542c9731ae7f3487b90cc74
vaishnavikshirsagar0811 <vaishnavi.kshirsagar@bhartisofttech.com>
2025-12-31 14:30:42 +0530

    js update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
 
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+
+
+const nextConfig = {
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'example.com',
+      'www.google.com',
+      'img.freepik.com',
+      'instaplex-optimize-image.s3.ap-south-1.amazonaws.com',
+      'images.unsplash.com',
+      'via.placeholder.com',
+    ],
+  },
+  
+  devIndicators: false,
+};
+ 
 export default nextConfig;
